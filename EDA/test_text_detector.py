a7f6a9e89da3b69299aab4218f4d267cb3815170
Toan <phanqu6ctoan@gmail.com>
2024-08-07 19:55:23 +0700

    feat: Update test text detector for single image path

--- EDA/test_text_detector.py
+++ EDA/test_text_detector.py
@@ -1,20 +1,27 @@
 from config import filtered_train_img_dir
 from utils.utility import get_random_img_path
 
-from paddleocr import PaddleOCR,draw_ocr
+from paddleocr import PaddleOCR, draw_ocr
 from PIL import Image
 import os
 
 
-ocr = PaddleOCR() # need to run only once to download and load model into memory
+def detect_text(img_path):
+    ocr = PaddleOCR()  # need to run only once to download and load model into memory
+    if img_path is None:
+        image_paths = get_random_img_path(filtered_train_img_dir, 10)
+    else:
+        image_paths = [img_path]
+    for img_path in image_paths:
+        result = ocr.ocr(img_path, cls=False, rec=False)
+        # draw result
+        result = result[0]
+        image = Image.open(img_path).convert('RGB')
+        im_show = draw_ocr(image, boxes=result)
+        im_show = Image.fromarray(im_show)
+        im_show.save(f'./text_detector_results/{os.path.basename(img_path)}')
 
-for idx, img_path in enumerate(get_random_img_path(filtered_train_img_dir, 10)):
-    result = ocr.ocr(img_path, cls=False, rec=False)
 
-    # draw result
-    result = result[0]
-    image = Image.open(img_path).convert('RGB')
-    im_show = draw_ocr(image, boxes=result)
-    im_show = Image.fromarray(im_show)
+if __name__ == '__main__':
+    detect_text("D:/ocr_receipts/data/train_images/mcocr_public_145013ahfqj.jpg")
 
-    im_show.save(f'./text_detector_results/{os.path.basename(img_path)}')
