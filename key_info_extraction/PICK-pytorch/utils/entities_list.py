3b2bcb7061a31da143298da5832a5c986fa79751
Toan <phanqu6ctoan@gmail.com>
2024-08-20 13:43:47 +0700

    Added PICK-pytorch as part of the project

---
+++ key_info_extraction/PICK-pytorch/utils/entities_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: Wenwen Yu
+# @Created Time: 7/8/2020 9:34 PM
+
+Entities_list = [
+    'SELLER',
+    'ADDRESS',
+    'TIMESTAMP',
+    'TOTAL_COST'
+]
+
+# Entities_list = [
+#     "ticket_num",
+#     "starting_station",
+#     "train_num",
+#     "destination_station",
+#     "date",
+#     "ticket_rates",
+#     "seat_category",
+#     "name"
+# ]
